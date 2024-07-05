86c82dd34f194947b8683a97639d97175db05cda
Bree Hoffman <bree@breefield.com>
2024-07-05 12:49:20 -0700

    Add server stuff, conflicting client and server now

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,52 @@ repositories {
 }
 
 wire {
+    custom {
+        // Be sure that `server-generator` is on the classpath for Gradle to resolve
+        // `GrpcServerSchemaHandler`.
+        schemaHandlerFactory = com.squareup.wire.kotlin.grpcserver.GrpcServerSchemaHandler.Factory()
+        options = mapOf(
+            // Defaults to `true` if absent. Any other value than `true` is considered false.
+            "singleMethodServices" to "false",
+            // Defaults to `suspending` if absent. Any other value than `suspending` is considered
+            // non-suspending.
+            "rpcCallStyle" to "blocking",
+        )
+        // We set the custom block exclusivity to false so that the next `kotlin {}` block can also
+        // generate the protobuf Messages.
+        exclusive = false
+    }
+
     sourcePath {
         srcDir("src/main/protos")
     }
 
-    kotlin {}
+    kotlin {
+        exclusive = false
+        rpcRole = "client"
+    }
+
+    kotlin {
+        exclusive = false
+        rpcCallStyle = "blocking"
+        rpcRole = "server"
+    }
+}
+
+dependencies {
+    api("com.squareup.wire:wire-runtime:5.0.0-alpha04")
+    implementation("com.squareup.wire:wire-grpc-client:5.0.0-alpha04")
+
+    // sever stuff
+    implementation("com.squareup.wiregrpcserver:server:1.0.0-alpha04")
+    implementation("io.grpc:grpc-stub:1.60.0")
+    implementation("io.grpc:grpc-protobuf:1.60.0")
+    implementation("io.grpc:grpc-netty:1.60.0")
+    implementation("io.grpc:grpc-services:1.60.0")
+}
+
+buildscript {
+    dependencies {
+        classpath("com.squareup.wiregrpcserver:server-generator:1.0.0-alpha04")
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,52 @@
 import com.bree.dinosaurs.Dinosaur
+import com.bree.dinosaurs.DinosaurTestClient
+import com.bree.dinosaurs.DinosaurTestWireGrpc
+import com.squareup.wire.GrpcClient
+
+import io.grpc.ServerBuilder
+import io.grpc.Status
+import io.grpc.StatusException
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.services.ProtoReflectionService
+import io.grpc.stub.StreamObserver
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
 
 fun main(args: Array<String>) {
-    println("Age:  ${Dinosaur(age = null).age}")
+    println("Age:  ${Dinosaur(age = null).age}`")
     println("Age:  ${Dinosaur(age = 1).age}")
+    println("Age:  ${Dinosaur(age = 0).age}")
     println("Name: ${Dinosaur().name}")
     println("Name: ${Dinosaur(name = "").name}")
+
+    val server = ServerBuilder.forPort(8080)
+        .addService(TestServer())
+        .addService(ProtoReflectionService.newInstance())
+        .build()
+
+    server.start()
+    server.awaitTermination()
+
+    val grpcClient = GrpcClient.Builder()
+        .client(OkHttpClient.Builder().protocols(listOf(Protocol.H2_PRIOR_KNOWLEDGE)).build())
+        .baseUrl("http:0.0.0.0:8080")
+        .build()
+
+    val client = grpcClient.create(DinosaurTestClient::class)
+
+}
+
+class TestServer : DinosaurTestWireGrpc.DinosaurTestImplBase() {
+    override fun Reflect(request: Dinosaur, response: StreamObserver<Dinosaur>) {
+        try {
+            response.onNext(request)
+            response.onCompleted()
+        } catch (e: StatusException) {
+            response.onError(e)
+        } catch (e: StatusRuntimeException) {
+            response.onError(e)
+        } catch (e: Exception) {
+            response.onError(StatusException(Status.INTERNAL.withCause(e).withDescription(e.toString())))
+        }
+    }
 }
